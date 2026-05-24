33176290a0a3df3732e211d73d8de500a2b66717
fabricioa27 <emersonarevalo77@gmail.com>
2026-05-23 18:17:47 -0600

    Proyecto creado por Spring initializr (la base)

---
+++ src/main/java/sv/edu/udb/Desafio03DwfApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio03DwfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio03DwfApplication.class, args);
+	}
+
+}
